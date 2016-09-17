91dc52ee90e7f9e70cdd7999cbe88834cabd3a1f
Tim Nosov <tnosov@v1020-wn-72-208.campus-dynamic.uwaterloo.ca>
2016-09-17 15:50:15 -0700

    added feature to parse google scholar

---
+++ parse_scholar.py
@@ -0,0 +1,21 @@
+# parse_scholar
+
+
+import scholar
+import os
+
+from subprocess import call
+
+from subprocess import call
+
+import subprocess
+# call(["ls", "-l"])
+
+
+os.system("python  scholar.py -a 'Yuri Shardt' --csv 1> yuri.csv")
+# x = subprocess.check_output(['whoami'])
+
+# print (x)
+
+
+# for i in range(20):
